5ef57bae3f9b040b9622aab9bbae41324fb0830c
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-02-18 16:49:04 +0100

    Remove test action

--- app/actions/__init__.py
+++ app/actions/__init__.py
@@ -1,13 +1,11 @@
 """Declare app actions."""
 
 from app.util_types import RunAction
-from .test import run
 from .generate_acuco_marker_image import generate_marker
 from .configure_perspective_correction import configure_perspective_correction
 
 
 actions: dict[str, RunAction] = {
-    'test': run,
     'Generate ArUco marker image': generate_marker,
     'Configure perspective correction': configure_perspective_correction
 }

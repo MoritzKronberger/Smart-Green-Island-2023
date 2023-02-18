f6ff7b749b9cacfd70dd710c95b4ca60b9814896
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-02-18 14:24:49 +0100

    Add action selection prompt

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,27 @@
-"""Run specified app action."""
+"""Run selected app actions."""
 
+import inquirer
 from .actions import actions
 
 
 def main() -> None:
-    """Run app action."""
-    actions['test']()
+    """Prompt action selection and run selected action."""
+    # Define selection for app actions using inquirer
+    # Reference: https://github.com/magmax/python-inquirer
+    actions_list = inquirer.List(
+        'action',
+        message='Launch Autonomous Ocean Garbage Collector action',
+        choices=actions.keys()
+    )
+    # Prompt selection
+    print(
+        '\n'
+        '######################################\n'
+        '# Autonomous Ocean Garbage Collector #\n'
+        '######################################\n'
+    )
+    answers = inquirer.prompt([actions_list])
+    # Run selected action
+    if answers:
+        action = answers['action']
+        actions[action]()
--- app/actions/__init__.py
+++ app/actions/__init__.py
@@ -1,15 +1,9 @@
 """Declare app actions."""
 
-from typing import TypedDict
-
 from app.util_types import RunAction
 from .test import run
 
 
-class __Actions(TypedDict):
-    test: RunAction
-
-
-actions = __Actions(
-    test=run
-)
+actions: dict[str, RunAction] = {
+    'test': run,
+}

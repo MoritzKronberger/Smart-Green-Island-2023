7a5725418c9fa1ce2c94110d51d46c688bb40091
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-02-26 11:48:48 +0100

    Refactor track boat action

--- app/actions/__init__.py
+++ app/actions/__init__.py
@@ -3,7 +3,7 @@
 from app.util_types import RunAction
 from .generate_acuco_marker_image import generate_marker
 from .configure_perspective_correction import configure_perspective_correction
-from .track_boat import track_boat
+from .autonomous_ocean_garbage_collector import autonomous_ocean_garbage_collector
 from .configure_blob_detection import configure_blob_detection
 
 
@@ -11,5 +11,5 @@ actions: dict[str, RunAction] = {
     'Generate ArUco marker image': generate_marker,
     'Configure perspective correction': configure_perspective_correction,
     'Configure blob detection': configure_blob_detection,
-    'Track boat': track_boat,
+    'Autonomous Ocean Garbage Collector': autonomous_ocean_garbage_collector,
 }
